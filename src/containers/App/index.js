52714942754f0331c9c707c3126ebe46067f1a1c
AJ Webb <webbushka@gmail.com>
2016-04-21 16:36:33 -0600

    Removed todo app for master branch

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,21 +3,16 @@ import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
-import Header from '../../components/Header';
-import MainSection from '../../components/MainSection';
-import * as TodoActions from '../../actions/todos';
 import style from './style.css';
 import favicon from './favicon.ico';
 import touchIcon from './apple-touch-icon-152x152.jpg';
 
 class App extends Component {
   render() {
-    const { todos, actions, children } = this.props;
+    const { children } = this.props;
     return (
-      <div className={style.normal}>
+      <div className={style.app}>
         <Helmet link={[ {rel: 'icon', href: favicon}, {rel: 'apple-touch-icon', href: touchIcon} ]} />
-        <Header addTodo={actions.addTodo} />
-        <MainSection todos={todos} actions={actions} />
         {children}
       </div>
     );
@@ -38,7 +33,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(TodoActions, dispatch)
+    actions: (bindActionCreators({/*should be your actions*/}, dispatch))
   };
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,8 @@
 
 import { routerReducer as routing } from 'react-router-redux';
 import { combineReducers } from 'redux';
-import todos from './todos';
 
 export default combineReducers({
   routing,
-  todos
+  // Your reducers go here
 });
